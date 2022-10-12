650534f6a8af4ea924818c48f5866eb2e2a9c531
Kshitiz <47204969+quantumbit1@users.noreply.github.com>
2022-10-12 11:05:35 +0530

    New update

--- client2.c
+++ client2.c
@@ -37,7 +37,43 @@ void clearBuf(char *b)
 char Cipher(char ch)
 {
     return ch ^ cipherKey; //Encryption using XOR
+
 }
+int recvFile_save(char *buf,int s,char *file_name)
+{
+    FILE *fp;
+    int i;
+    char ch;
+    char file_error[] = "File_Not_Found";
+    char base_address[FILE_SIZE] = "./users-drive-download-files/";
+    strcat(base_address,file_name);
+/*    if(strcmp(buf,file_error)==0)
+    {
+        return;
+    }
+*/
+    fp = fopen(base_address,"wb");
+    
+    for(i = 0;i<s;i++)
+    {
+        ch = buf[i];
+        ch = Cipher(ch);
+        if(ch == EOF)
+        {
+            return 1;
+        }
+        else
+        {
+            fputc(ch,fp);
+            printf("%c",ch);
+            //sleep(2);
+        }
+    }
+
+    fclose(fp);
+    return 0;
+}
+
 int recvFile(char *buf,int s)
 {
     int i;
@@ -56,71 +92,63 @@ int recvFile(char *buf,int s)
             //sleep(2);
         }
     }
+
     return 0;
 }
-void save_file(FILE *fp,char *buf,int s,char *file_name)
+int file_found(char *buf,int s)
 {
     char file_error[] = "File_Not_Found";
-    char base_address[FILE_SIZE] = "./users-drive-download-files/";
-    strcat(base_address,file_name);
     char string[strlen(buf) - 1];
     int i = 0;
     for(i=0;i<strlen(buf)-1;i++)
+    {
         string[i] = Cipher(buf[i]);
+    }
+
+    printf("String: %s\n",string);
     if(strcmp(string,file_error)==0)
     {
-        return;
+        return -1;
     }
 
-    fp = fopen(base_address,"wb");
-    string[i] = '\0';
-
-    printf("\nFile Name Received: %s\n",file_name);
-
-    printf("File Error: %s SIZE OF STRING: %ld STRLEN OF BUF %ld STRLEN of STRING %ld STRCMP %d STRING %s\n\n\n",file_error,sizeof(string),strlen(buf),strlen(string),strcmp(string,file_error),string);
-    if(fp ==NULL)
-    {
-        printf("\nDownload failed!!\n");
-    }
-    else
-    {
-        fwrite(string,sizeof(char),sizeof(string),fp);
-        printf("\nDownload  Successfull!!\n");
-    }
-    fclose(fp);
-
+   return 0;
 }
 void Download(int sockfd,char *net_buf,struct sockaddr_in addr_con,int addrlen,char *file_name)
 {
     int nBytes;
     FILE *fp;
+    clearBuf(net_buf);
     strcpy(net_buf,PUSH_DOWNLOAD_FILE);
     send(sockfd,net_buf,NET_BUF_SIZE,sendrecvflag);
+    clearBuf(net_buf);
+    printf("\nPlease Enter file name....\n");
+    scanf("%s",net_buf);
+    strcpy(file_name,net_buf);
+    if(strcmp(net_buf,EXIT)==0)
+    {
+        return;
+    }
+    send(sockfd,net_buf,NET_BUF_SIZE,sendrecvflag);
+    printf("\n***********Data Received************\n");
 
-        printf("\nPlease Enter file name....\n");
-        scanf("%s",net_buf);
-        strcpy(file_name,net_buf);
-        if(strcmp(net_buf,EXIT)==0)
+    while(1)
+    {
+        clearBuf(net_buf);
+        nBytes = recv(sockfd,net_buf,NET_BUF_SIZE,sendrecvflag);
+
+
+        /*saving file at client side*/
+        if(file_found(net_buf,NET_BUF_SIZE)==-1)
         {
-            return;
+            break;
         }
-        send(sockfd,net_buf,NET_BUF_SIZE,sendrecvflag);
-        printf("\n***********Data Received************\n");
 
-        while(1)
+        if(recvFile_save(net_buf,NET_BUF_SIZE,file_name))
         {
-            clearBuf(net_buf);
-            nBytes = recv(sockfd,net_buf,NET_BUF_SIZE,sendrecvflag);
-
-            /*saving file at client side*/
-            save_file(fp,net_buf,NET_BUF_SIZE,file_name);
-
-            if(recvFile(net_buf,NET_BUF_SIZE))
-            {
-                break;
-            }
+            break;
         }
-        printf("\n************************************\n"); 
+    }
+    printf("\n************************************\n"); 
 }
 
 void EndConnection(int sockfd,char *net_buf,struct sockaddr_in addr_con,int addrlen)
@@ -133,9 +161,11 @@ void EndConnection(int sockfd,char *net_buf,struct sockaddr_in addr_con,int addr
 void ListAllDriveFiles(int sockfd,char *net_buf,struct sockaddr_in addr_con,int addrlen)
 {
     int nBytes;
+    clearBuf(net_buf);
     strcpy(net_buf,LIST_DIR);
     printf("Inside List Drive\n");
     send(sockfd,net_buf,NET_BUF_SIZE,sendrecvflag);
+    clearBuf(net_buf);
     printf("\n***********Data Received************\n");
 
     while(1)
@@ -145,7 +175,8 @@ void ListAllDriveFiles(int sockfd,char *net_buf,struct sockaddr_in addr_con,int 
 
         /*saving file at client side*/
         //save_file(fp,net_buf,NET_BUF_SIZE,file_name);
-
+        //
+        //printf("Recieved Directory List %s\n",net_buf)
         if(recvFile(net_buf,NET_BUF_SIZE))
         {
             break;
@@ -208,7 +239,6 @@ int main()
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(PORT_NO);
 
-    // 127.0.0.1 is Loopback IP
     serverAddr.sin_addr.s_addr
         = inet_addr(IP_ADDRESS);
 
